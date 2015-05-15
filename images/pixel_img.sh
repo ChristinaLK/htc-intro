e403b92702553a80988a747e2086d2c96472ea73
Christina Koch <christinakconnect@gmail.com>
2015-05-15 13:05:49 -0500

    updating image script and submit

--- images/pixel_img.sh
+++ images/pixel_img.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-name=$1
-fullname=$*
-
-tar xzf cowsay-3.03.tar.gz 
-cd cowsay-3.03
-./install.sh ./
-bin/cowsay Hello $fullname >> ../$1_greeting.txt
+## Get python and build
+wget http://proxy.chtc.wisc.edu/SQUID/ckoch5/Python-2.7.7.tgz
+tar xzf Python-2.7.7.tgz
+cd Python-2.7.7
+DIR=$(pwd)
+./configure 
+make altinstall prefix=$DIR exec-prefix=$DIR
 cd ..
-rm -rf cowsay-3.03/
+alias python="$DIR/python"
+
+## Run python code   
+#wget files from above, unpack, run
+
+## Remove files
+rm Python-2.7.7.tgz
+rm -rf Python-2.7.7
