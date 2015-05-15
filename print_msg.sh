5f5ffb8c019bf8a5490ca2e48a2f72f1c0351441
Christina Koch <christinakconnect@gmail.com>
2015-05-15 13:06:29 -0500

    modifying workflow to dl from squid

--- print_msg.sh
+++ print_msg.sh
@@ -3,9 +3,16 @@
 name=$1
 fullname=$*
 
+#dl and install cowsay
+wget http://proxy.chtc.wisc.edu/SQUID/ckoch5/cowsay-3.03.tar.gz 
 tar xzf cowsay-3.03.tar.gz 
 cd cowsay-3.03
 ./install.sh ./
+
+#print message
 bin/cowsay Hello $fullname >> ../$1_greeting.txt
+
+#remove files
 cd ..
+rm cowsay-3.03.tar.gz 
 rm -rf cowsay-3.03/
